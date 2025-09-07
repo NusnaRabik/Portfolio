e49e4e09ec645e7a3c61bd784a4863f12c564df7
NusnaRabik <fathimnza@gmail.com>
2025-09-07 12:49:40 +0530

    Setup portfolio for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Nuz-Portfolio/', // <-- your repo name
+  base: '/NuzPortfolio/', // <-- your repo name
 })
